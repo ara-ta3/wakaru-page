df71ae02c4fb424038b3fb14078d98cc50950285
Arata Tanaka <tarata43@yahoo.co.jp>
2015-04-11 15:18:57 +0900

    色々揃えた

---
+++ Makefile
@@ -0,0 +1,2 @@
+start:
+	php -S localhost:9000 -t public_html
---
+++ public_html/js/app.js
@@ -0,0 +1,9 @@
+(function() {
+    $(document).cheatCode({
+        message : 'わかる'
+    });
+    $('.text').chaffle({
+        speed: 1, //速度
+        time: 60 //時間
+    });
+})();
